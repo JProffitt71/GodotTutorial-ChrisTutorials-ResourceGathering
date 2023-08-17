1da99577eeedfac8f178de2e1450ac6a26723a3b
Jessie Proffitt <jproffitt71@gmail.com>
2023-08-16 19:25:05 -0600

    Added tilemap, some level design

--- UI/ResourceDisplay.gd
+++ UI/ResourceDisplay.gd
@@ -2,7 +2,7 @@ extends MarginContainer
 
 @export var item_display_template : PackedScene
 
-@onready var grid : GridContainer = $Grid
+@onready var grid : GridContainer = $Panel/Grid
 
 var displays : Array[ResourceItemDisplay] = [] 
 var player_inventory : Inventory
@@ -13,6 +13,10 @@ func _ready():
 	
 	player_inventory = player.find_child("Inventory") as Inventory
 	player_inventory.connect("resource_count_changed", _on_player_inventory_resource_count_changed)
+	
+	# Hide inventory initially
+	
+	self.visible = false
 
 
 func _on_player_inventory_resource_count_changed(type : ResourceItem, new_count : int):
@@ -36,6 +40,10 @@ func _on_player_inventory_resource_count_changed(type : ResourceItem, new_count 
 	# Update display item for resource with new count
 	
 	current_display.update_count(new_count)
+	
+	# Show inventory now that we have items
+	
+	self.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
